4f823674c3e5496c6f1149dceef6d0ba75a0ea2e
Hilbert Ruban <hilbert.r@targetbay.com>
2024-04-15 00:08:52 +0530

    added middleware to contain user id for message controller

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 import dotenv from "dotenv";
 import express from "express";
+import cookieParser from "cookie-parser";
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import authRoutes from "./routes/authRoutes.js";
+import messageRoute from "./routes/messageRoutes.js";
 
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -10,8 +12,10 @@ const PORT = process.env.PORT || 8080;
 dotenv.config();
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoute);
 
 app.listen(PORT, () => {
     connectToMongoDB();
